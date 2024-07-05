a03d0cbfb9ce533dcec665853c1ae9a44b2b4e42
Shufanchen <86466932+shufanchen@users.noreply.github.com>
2024-07-05 14:59:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,34 @@ logger.addHandler(file_handler)
 user_id = str(uuid.uuid4())
 logger.info(f"New session started with user ID: {user_id}")
 
+
+
 base_path = './RAG_models'
+
+# 检查目标目录是否存在，如果不存在则克隆仓库
+if not os.path.exists(base_path):
+    clone_command = f'git clone https://ent-app-dev:1ca51f9b37ac0c2ecfdaeb509718ec5ca39835c3@code.openxlab.org.cn/ent-app-dev/RAG_models.git {base_path}'
+    clone_result = os.system(clone_command)
+    if clone_result != 0:
+        raise RuntimeError(f"Failed to clone repository with command: {clone_command}")
+
+    # 安装 Git LFS
+    lfs_install_command = 'git lfs install'
+    print(f"Running LFS install command: {lfs_install_command}")
+    lfs_install_result = os.system(lfs_install_command)
+    if lfs_install_result != 0:
+        raise RuntimeError(f"Failed to install Git LFS with command: {lfs_install_command}")
+
+    # 拉取 LFS 文件
+    lfs_pull_command = f'cd {base_path}/Sft_model && git lfs pull'
+
+    try:
+        lfs_pull_result = subprocess.run(lfs_pull_command, shell=True, check=True, capture_output=True)
+        print(f"LFS pull output for: {lfs_pull_result.stdout.decode()}")
+    except subprocess.CalledProcessError as e:
+        print(f"LFS pull error output: {e.stderr.decode()}")
+        raise RuntimeError(f"Failed to pull LFS files with command: {lfs_pull_command}")
+
 
 tokenizer = AutoTokenizer.from_pretrained(base_path + '/Sft_model', trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(base_path + '/Sft_model', trust_remote_code=True).cuda()
